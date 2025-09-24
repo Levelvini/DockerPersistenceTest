dcb2b03edbab9e4e2808eb5cd7f1722a693555e7
Levelvini <viniciusfc29@gmail.com>
2025-09-24 20:33:48 -0300

    project start

---
+++ src/main/java/com/levelVini/DockerPersistenceTest/DockerPersistenceTestApplication.java
@@ -0,0 +1,13 @@
+package com.levelVini.DockerPersistenceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPersistenceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPersistenceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelVini/DockerPersistenceTest/DockerPersistenceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelVini.DockerPersistenceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPersistenceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

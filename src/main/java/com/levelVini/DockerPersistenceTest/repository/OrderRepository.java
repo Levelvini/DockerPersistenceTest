6c106c33a94be15facd1c39984d19bdcdc63d5b9
Levelvini <viniciusfc29@gmail.com>
2025-09-30 19:44:50 -0300

    feat:repositories created

---
+++ src/main/java/com/levelVini/DockerPersistenceTest/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.levelVini.DockerPersistenceTest.repository;
+
+import com.levelVini.DockerPersistenceTest.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}

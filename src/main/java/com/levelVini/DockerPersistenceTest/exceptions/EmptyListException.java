4295bb4c0d15b9b35c11464de05b86222748a06e
Levelvini <viniciusfc29@gmail.com>
2025-11-05 14:10:26 -0300

    feat: EmptyListException has been created

---
+++ src/main/java/com/levelVini/DockerPersistenceTest/exceptions/EmptyListException.java
@@ -0,0 +1,7 @@
+package com.levelVini.DockerPersistenceTest.exceptions;
+
+public class EmptyListException extends RuntimeException{
+    public EmptyListException(String message) {
+        super(message);
+    }
+}

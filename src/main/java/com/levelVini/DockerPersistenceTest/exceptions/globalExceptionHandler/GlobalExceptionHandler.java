f3acdd4d9237b9222127fab5784c4f2066492bc9
Levelvini <viniciusfc29@gmail.com>
2025-12-09 21:04:45 -0300

    feat: add treatment to Spring validator exceptions

--- src/main/java/com/levelVini/DockerPersistenceTest/exceptions/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/levelVini/DockerPersistenceTest/exceptions/globalExceptionHandler/GlobalExceptionHandler.java
@@ -11,14 +11,13 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> Exception(Exception ex) {
